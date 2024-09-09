1180597ff526481ba8092b9e5520da19f6972016
Eugene Chintsov <tr.e.chintsov@gmail.com>
2024-09-09 14:31:46 +0700

    modified:   brain_games/__init__.py 	new file:   brain_games/cli.py 	modified:   brain_games/scripts/__init__.py 	modified:   brain_games/scripts/brain_games.py 	modified:   poetry.lock 	modified:   pyproject.toml

---
+++ brain_games/cli.py
@@ -0,0 +1,9 @@
+#from prompt_toolkit import prompt
+
+
+def welcome_user():
+    print('Welcome to the Brain Games!')
+    #name = prompt.string('May I have yout name? ')
+    print(f'Hello,name!')
+
+#welcome_user()
---
+++ brain_games/scripts/__init__.py
@@ -0,0 +1,1 @@
+Name = 'scripts package'
