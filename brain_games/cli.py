2706aae8673fe01ff0167e83c9d53458e6845cdc
Eugene Chintsov <tr.e.chintsov@gmail.com>
2024-09-10 21:26:42 +0700

    include prompt package to cli
    	modified:   brain_games/cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,9 +1,7 @@
-#from prompt_toolkit import prompt
+import prompt
 
 
 def welcome_user():
     print('Welcome to the Brain Games!')
-    #name = prompt.string('May I have yout name? ')
-    print(f'Hello,name!')
-
-#welcome_user()
+    name = prompt.string('May I have yout name? ')
+    print(f'Hello,{name}!')
