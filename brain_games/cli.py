950eba66718e935cb6f102bf8328075ba07e1c32
Eugene Chintsov <tr.e.chintsov@gmail.com>
2024-09-26 02:39:59 +0700

    Fix: welcome_user()
    	modified:   brain_games/cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -6,4 +6,4 @@ def welcome_user():
     '''Read username and print greetings'''
     print('Welcome to the Brain Games!')
     username = prompt.string('May I have your name? \n')
-    print(f'Hello,{username}!')
+    print(f'Hello, {username}!')
