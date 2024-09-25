a4522fb66e4bb73b4848cf55cca8924bb3addf33
Eugene Chintsov <tr.e.chintsov@gmail.com>
2024-09-26 02:59:41 +0700

    Fix: variable 'question' in function get_parametres
    	modified:   brain_games/games/gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -20,5 +20,5 @@ def get_parametres() -> tuple[str, str]:
     number1 = randint(MIN_NUMBER, MAX_NUMBER)
     number2 = randint(MIN_NUMBER, MAX_NUMBER)
     correct_answer = str(abs(gcd(number1, number2)))
-    question = f'{number1} and {number2}'
+    question = f'{number1} {number2}'
     return question, correct_answer
