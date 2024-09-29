997fd38ec0e4be96087b2d667711482a5422b3a1
Eugene Chintsov <tr.e.chintsov@gmail.com>
2024-09-30 05:27:23 +0700

    Refactor:
    	modified:   brain_games/engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -5,22 +5,21 @@ import prompt
 QUESTION_COUNT = 3
 
 
-def run(game):
-    """Launches the game using the passed game parameters."""
-    description = game.DESCRIPTION
+def run(game: any) -> None:
+    """Launches the game."""
     print('Welcome to the Brain Games!')
     username = prompt.string('May I have your name? \n')
     print(f'Hello, {username}!')
-    print(description)
-    for n in range(QUESTION_COUNT):
-        question, correct_answer = game.get_parameters()
+    print(game.DESCRIPTION)
+    for _ in range(QUESTION_COUNT):
+        question, correct_answer = game.generate_game()
         print(f'Question: {question} ')
         answer = prompt.string('Your answer: ')
-        if correct_answer == answer:
-            print('Correct!')
-        else:
-            print(f'"{answer}" is wrong answer ;(. '
-                  f'Correct answer was "{correct_answer}".\n'
-                  f'Let\'s try again, {username}!')
+        if answer != correct_answer:
+            print(
+                f'"{answer}" is wrong answer ;(.'
+                f'Correct answer was "{correct_answer}".\n'
+                f'Let\'s try again, {username}!')
             return
+        print('Correct!')
     print(f'Congratulations, {username}!')
