fb7c2a966d90f9ea7110f6b8d33df9ec6d459208
Eugene Chintsov <tr.e.chintsov@gmail.com>
2024-09-30 00:27:18 +0700

    Refactor:
    	modified:   brain_games/games/gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -7,18 +7,15 @@ MAX_NUMBER = 10
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
-def gcd(num1: int, num2: int) -> int:
+def find_gcd(num1: int, num2: int) -> int:
     """Determines the greatest common divisor of two numbers."""
-    if num2 == 0:
-        return num1
-    else:
-        return gcd(num2, num1 % num2)
+    return num1 if num2 == 0 else find_gcd(num2, num1 % num2)
 
 
-def get_parameters() -> tuple[str, str]:
+def generate_game() -> tuple[str, str]:
     """Release game logic and return parameters for game engine"""
     number1 = randint(MIN_NUMBER, MAX_NUMBER)
     number2 = randint(MIN_NUMBER, MAX_NUMBER)
-    correct_answer = str(abs(gcd(number1, number2)))
+    correct_answer = str(abs(find_gcd(number1, number2)))
     question = f'{number1} {number2}'
     return question, correct_answer
