abb4ea0fb7c0bd90c13a658e8b6a897ad34d2940
Eugene Chintsov <tr.e.chintsov@gmail.com>
2024-09-30 17:13:20 +0700

    Refactor:
    	modified:   brain_games/engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -9,8 +9,10 @@ def run(game: any) -> None:
     """Launches the game."""
     print('Welcome to the Brain Games!')
     username = prompt.string('May I have your name? \n')
-    print(f'Hello, {username}!')
-    print(game.DESCRIPTION)
+    print(
+        f'Hello, {username}!\n'
+        f'{game.DESCRIPTION}'
+    )
     for _ in range(QUESTION_COUNT):
         question, correct_answer = game.generate_game()
         print(f'Question: {question} ')
