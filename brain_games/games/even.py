e0395c44492a23e8f74086d491ddf2c16320665b
Eugene Chintsov <tr.e.chintsov@gmail.com>
2024-09-30 17:03:34 +0700

    Refactor:
    	modified:   brain_games/games/even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -7,14 +7,14 @@ MAX_NUMBER = 10
 DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def is_even(number: int) -> str:
+def is_even(number: int) -> bool:
     """Determines whether a number is even and returns the answer."""
-    return 'yes' if number % 2 == 0 else 'no'
+    return True if number % 2 == 0 else False
 
 
 def generate_game() -> tuple[str, str]:
     """Release game logic and return parameters for game engine"""
     number = randint(MIN_NUMBER, MAX_NUMBER)
-    correct_answer = is_even(number)
+    correct_answer = 'yes' if is_even(number) else 'no'
     question = str(number)
     return question, correct_answer
